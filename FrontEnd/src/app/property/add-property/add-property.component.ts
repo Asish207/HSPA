414c87103ca78f6e2f9412cf8c0e34e9cff4b08e
Abhishek-Subba <abhishek.subba@classteacher.com>
2021-06-13 20:55:35 +0530

    13/06/21

---
+++ FrontEnd/src/app/property/add-property/add-property.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms'
+
+@Component({
+  selector: 'app-add-property',
+  templateUrl: './add-property.component.html',
+  styleUrls: ['./add-property.component.css']
+})
+export class AddPropertyComponent implements OnInit {
+  @ViewChild('Form') addPropertyForm : NgForm;
+  constructor(private router : Router) { }
+
+  ngOnInit(): void {
+  }
+
+  onBack(){
+    this.router.navigate(['/']);
+  }
+
+  onSubmit(){
+    console.log('congrats forms submitted') ;
+    console.log(this.addPropertyForm) ;
+  }
+
+}
--- FrontEnd/src/app/property/property-list/property-list.component.ts
+++ FrontEnd/src/app/property/property-list/property-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HousingService } from 'src/app/services/housing.service';
+import { IProperty } from '../IProperty.interface';
 
 @Component({
   selector: 'app-property-list',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-list.component.css']
 })
 export class PropertyListComponent implements OnInit {
-
-  constructor() { }
+  SellRent = 1;
+  properties : Array<IProperty>;
+  constructor(private housingService : HousingService, private route : ActivatedRoute) { }
 
   ngOnInit(): void {
+    if(this.route.snapshot.url.toString()){
+      this.SellRent = 2;
+    }
+    this.housingService.getAllProperties(this.SellRent).subscribe(
+      data =>{
+        this.properties = data;
+        console.log(data);
+      },
+      error =>{
+        console.log('httpError');
+        console.log(error);
+      }
+
+    )
   }
 
 }
